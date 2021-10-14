7e7d13f46967108162974f8b9293a58f2c2c5cc1
Nguyen Quang Trong <quangtronggg@gmail.com>
2021-10-14 23:44:57 +0700

    mofify vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-      ? 'Student_Management' // Thay tên repository của các bạn vào đây nhé
+      ? '/Student_Management/' // Thay tên repository của các bạn vào đây nhé
       : '/'
   }
